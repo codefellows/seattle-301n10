c4fd7dbb21e26992992013dd4258b6144f5b0a9b
JB Tellez <jb.tellez@gmail.com>
2018-03-08 15:43:07 -0800

    removes demo files

--- 03-jquery-with-events/demos/sort.js
+++
@@ -1,8 +0,0 @@
-let myArray = [1, 5, 82, 112, 44];
-
-myArray.sort();
-
-myArray.sort(function (a, b) {
-  console.log(myArray);
-  return a - b;
-})
--- 12-client-side-routing/pagejs/scripts/controllers/routes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-page('/', homeView.init);
-page('/about', aboutView.init);
-page('/contact', contactView.init);
-page('/contact/:number', contactView.init);
-
-page();
